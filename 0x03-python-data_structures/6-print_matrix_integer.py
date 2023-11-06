004c9bd33a3ad25ab7765bd681a7d2d828e782d4
iadekunle3 <iadekunle34@gmail.com>
2023-11-06 09:33:11 -0800

    check matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{0}".format(num), end=" ")
+        for elem in row:
+            if elem == row[-1]:
+                print("{:d}".format(elem), end="")
+            else:
+                print("{:d}".format(elem), end=" ")
         print()
