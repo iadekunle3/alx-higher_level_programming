7b0c9014bdb0a748d4e21261d693b37ab14c9243
iadekunle3 <iadekunle34.com>
2023-11-20 21:37:15 +0000

    check all

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            n = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            n = 0
+        except TypeError:
+            print("wrong type")
+            n = 0
+        except IndexError:
+            print("out of range")
+            n = 0
+        finally:
+            result.append(n)
     return result
