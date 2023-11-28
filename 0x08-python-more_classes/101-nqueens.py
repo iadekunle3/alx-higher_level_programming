2e28152a2c6893d73584873503797b13a2327587
iadekunle3 <iadekunle34.com>
2023-11-28 16:15:57 +0000

    check

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-"""
-
-This module contains an algorithm that resolves the N-Queen puzzle
-using backtracking
-
-"""
-
-
-def solveNQueen(n):
-""" Method that determines if the queens can or can't kill each other
-
-    Args:
-        m_queen: array that has the queens positions
-        nqueen: queen number
-
-    Returns:
-        True: when queens can't kill each other
-        False: when some of the queens can kill
-
-    """
-    def isSafe(queen_number, position):
+# Function to solve the N-Queens problem
+def solve_n_queen(n):
+    # Nested function to check if a queen can be placed at a given position
+    def is_safe(queen_number, position):
+        # Loop over all queens to the left of the current queen
         for i in range(queen_number):
-            if queens[i] == position or \
-                queens[i] - i == position - queen_number or \
-                queens[i] + i == position + queen_number:
+            # Check if another queen is in the same row or on the same diagonal
+            if (queens[i] == position or
+                    queens[i] - i == position - queen_number or
+                    queens[i] + i == position + queen_number):
                 return False
         return True
 
-    def placeQueen(queen_number, n):
+    # Nested function to place queens one by one, starting from the left
+    def place_queen(queen_number, n):
+        # If all queens are placed correctly, print the solution
         if queen_number == n:
             result.append(queens[:])
             return
+        # Try to place a queen in each row of the current column
         for i in range(n):
-            if isSafe(queen_number, i):
+            # If the queen can be placed at the current position, place it
+            if is_safe(queen_number, i):
                 queens[queen_number] = i
-                placeQueen(queen_number + 1, n)
+                # Place the rest of the queens
+                place_queen(queen_number + 1, n)
 
+    # Initialize the result list and the queens list
     result = []
     queens = [-1] * n
-    placeQueen(0, n)
+    # Start placing queens from the first column
+    place_queen(0, n)
     return result
