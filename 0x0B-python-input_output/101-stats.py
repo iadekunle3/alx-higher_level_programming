9a03b41edbbe5b07b44a194f41a3c2c2ef768777
iadekunle3 <iadekunle34.com>
2023-12-06 19:39:11 +0000

    check

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,47 +1,15 @@
 #!/usr/bin/python3
-"""Intialize counters"""
-
+import random
 import sys
-import signal
+from time import sleep
+import datetime
 
-total_size = 0
-status_codes = {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
-
-"""Function to handle CTRL+C interruption"""
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
-
-"""Function to print statistics"""
-def print_statistics():
-    print("Total file size: File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-
-"""Attach the signal handler for SIGINT (CTRL+C)"""
-signal.signal(signal.SIGINT, signal_handler)
-
-"""Read from stdin and process lines"""
-try:
-    for line_number, line in enumerate(sys.stdin, 1):
-        parts = line.split()
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-
-        """Update total size and status code count"""
-        total_size += file_size
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-
-        """Print statistics every 10 lines"""
-        if line_number % 10 == 0:
-            print_statistics()
-
-except KeyboardInterrupt:
-    """Handle any additional CTRL+C interruptions"""
-    pass
-
-"""Print final statistics if not a multiple of 10 lines"""
-if line_number % 10 != 0:
-    print_statistics()
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
