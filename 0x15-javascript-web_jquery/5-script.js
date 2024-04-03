f17e87b05c97ca1c2055bf301bde444b9430a3d7
Iadekunle3 <Iadekunle34@gmail.com>
2024-04-03 17:50:48 -0400

    jquery check

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,10 @@
+<script>
+
+const $listElem = $('ul.my_list');
+const $addItemElem = $('div#add_item');
+
+$addItemElem.on('click', () => {
+  $listElem.append('<li>Item</li>');
+});
+
+</script>
---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,10 @@
+<script>
+  
+  const $headerElem = $('header');
+  const $updateHeaderElem = $('div#update_header');
+
+  $updateHeaderElem.on('click', () => {
+  $headerElem.text('New Header!!!');
+  });
+
+</script>
